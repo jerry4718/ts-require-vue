8d1915e96d37cd74935be7c3a935ece20cf3b030
jerry4718 <jerry4718@qq.com>
2020-07-16 18:08:29 +0800

    启动代码修改

--- src/boot.js
+++ src/boot.js
@@ -1,8 +1,3 @@
-require(["require.config"], function(config) {
-    require.config(config);
-    console.log(1, arguments);
-    require(["text!app/App.ts", "Vue"], function (template, vue) {
-        console.log(template, vue);
-
-    })
+require(["require.config"], function() {
+    require(["app/App"])
 });
